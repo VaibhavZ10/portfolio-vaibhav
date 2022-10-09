4a8ac47b31280ce143a06e0b8fb0e18f56f0713b
vaibhav <vaibhav.sindal@gmail.com>
2022-10-09 13:56:01 +0530

    nav inital

---
+++ components/Navbar.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+import styles from "../styles/nav.module.scss";
+
+import React from "react";
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  let navbarClasses = ["navBar"];
+  if (scrolled) {
+    navbarClasses.push("scrolled");
+  }
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.navWrapper}>
+        <div className={styles["logo"]}>
+          <a href="#">VS</a>
+        </div>
+        <ul className={styles["nav-options"]}>
+          <li className={styles.option}>
+            <a href="#">About</a>
+          </li>
+
+          <li className={styles.option}>
+            <a href="#">Services</a>
+          </li>
+          <li className={styles.option}>
+            <a href="#">Portfolio</a>
+          </li>
+          <li className={styles.option}>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- components/Work.js
+++ components/Work.js
@@ -5,12 +5,12 @@ const Services = () => {
   return (
     <div className={styles.contentContainer}>
       <h1>
-        What I Offer <br></br>
-        <span>My Servcies</span>
+        Recent Works <br></br>
+        <span></span>
       </h1>
       <div className={styles.boxContainer}>
         <div className={styles.boxDesign}>
-          <h5 bold>#Creation of a website/web app design </h5>
+          <h5 bold>Task Tracker using React </h5>
           <p>
             You want to launch your website, you need a design for your future
             site? Reach out to me and we will discuss your needs and desires.
@@ -18,7 +18,7 @@ const Services = () => {
           <button className={styles.btn}>Contact Me</button>
         </div>
         <div className={styles.boxDesign}>
-          <h5 bold>#Rebranding </h5>
+          <h5 bold>Breast Cancer Detection </h5>
           <p>
             You want to refurbish your website, need a new and sleek design?
             Reach out to me and we will discuss your needs and desires.
